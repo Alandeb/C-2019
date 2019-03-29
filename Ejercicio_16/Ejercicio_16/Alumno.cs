9aad089672cc9e7f03f819fa5d6dd3dbe5e3410b
Alan_Debonis <alan_debonis@hotmail.com>
2019-03-28 22:06:41 -0300

    Clase 4 28/03/19

--- Ejercicio_16/Ejercicio_16/Alumno.cs
+++ Ejercicio_16/Ejercicio_16/Alumno.cs
@@ -23,20 +23,50 @@ namespace Ejercicio_16
             apellido = " ";
             nombre = " ";
         }
-        public Alumno (byte nota1 , byte nota2)
+        public Alumno (byte nota1 , byte nota2)//constructor
         {
             this.nota1 = nota1;
             this.nota2 = nota2;
         }
-
-
-    }
-    
-    public class Prueba
-    {
-        Alumno prueba = new Alumno();
-        Alumno prueba2 = new Alumno()
+        public Alumno (int leg, string nom ,string ape)// constructor del punto A
+        {
+            this.legajo = leg;
+            this.nombre = nom;
+            this.apellido = ape;
+        }
+                    
+        public void Estudiar (byte nota1 , byte nota2)
+        {
+            this.nota1 = nota1;
+            this.nota2 = nota2;
             
+        }
+        public void CalcularFinal()
+        {
+            if (nota1 >= 4 && nota2 >= 4)
+            {
+                Random objram = new Random();
+                this.notaFinal=objram.Next(4, 10);
+
+            }
+            else
+            {
+                this.notaFinal = -1;
+            }
+        }
+        public string Mostrar()
+        {
+            if (notaFinal == -1)
+            {
+                return string.Format("Nota 1: {0}, Nota 2: {1}, Nota final: Desaprobado, " +
+                    "Legajo: {2}, Nombre: {3}, Apellido: {4} ", nota1,nota2,legajo,nombre,apellido);
+            }
+            else
+            {
+                return string.Format("Nota 1: {0}, Nota 2: {1}, Nota final: {2}, " +
+                    "Legajo: {3}, Nombre: {4}, Apellido: {5}", nota1, nota2,notaFinal, legajo, nombre, apellido);
+            }
+        }
     }
 
 }
--- Ejercicio_16/Ejercicio_16/Program.cs
+++ Ejercicio_16/Ejercicio_16/Program.cs
@@ -10,6 +10,126 @@ namespace Ejercicio_16
     {
         static void Main(string[] args)
         {
+            Alumno student1 = new Alumno(1146 , "alan" , "debonis");
+            Alumno student2 = new Alumno(1045 , "hernan" , "pato");
+            Alumno student3 = new Alumno(1080, "juan" , "domingo");
+            string parseint;
+            int  flagnota1 = 0, flagnota2 = 0, numero ;
+            byte nota1 = 0 ,nota2 = 0 ;
+            
+                do
+                {
+                    Console.Write("Ingrese un nota1: ");
+                    parseint = Console.ReadLine();
+                    if ((int.TryParse(parseint, out numero)) && numero >= 0 && numero <= 10)
+                    {
+                        flagnota1 = 1;
+                        nota1 = (byte)numero;
+
+                        do
+                        {
+                            Console.Write("Ingrese un nota2: ");
+                            parseint = Console.ReadLine();
+                            if ((int.TryParse(parseint, out numero)) && numero >= 0 && numero <= 10)
+                            {
+                                flagnota2 = 1;
+                                nota2 = (byte)numero;
+                        }
+                            else
+                            {
+                                Console.WriteLine("ERROR !!");
+                            }
+                        } while (flagnota2 == 0);
+                    }
+                    else
+                    {
+                        Console.WriteLine("ERROR !!");
+                    }
+
+                } while (flagnota1 == 0 && flagnota2 == 0);
+            student1.Estudiar(nota1, nota2);
+
+            nota1 = 0;
+            nota2 = 0;
+            flagnota1 = 0;
+            flagnota2 = 0;
+            do
+            {
+                Console.Write("Ingrese un nota1: ");
+                parseint = Console.ReadLine();
+                if ((int.TryParse(parseint, out numero)) && numero >= 0 && numero <= 10)
+                {
+                    flagnota1 = 1;
+                    nota1 = (byte)numero;
+
+                    do
+                    {
+                        Console.Write("Ingrese un nota2: ");
+                        parseint = Console.ReadLine();
+                        if ((int.TryParse(parseint, out numero)) && numero >= 0 && numero <= 10)
+                        {
+                            flagnota2 = 1;
+                            nota2 = (byte)numero;
+                        }
+                        else
+                        {
+                            Console.WriteLine("ERROR !!");
+                        }
+                    } while (flagnota2 == 0);
+                }
+                else
+                {
+                    Console.WriteLine("ERROR !!");
+                }
+
+            } while (flagnota1 == 0 && flagnota2 == 0);
+
+            student2.Estudiar(nota1, nota2);
+            nota1 = 0;
+            nota2 = 0;
+            flagnota1 = 0;
+            flagnota2 = 0;
+            do
+            {
+                Console.Write("Ingrese un nota1: ");
+                parseint = Console.ReadLine();
+                if ((int.TryParse(parseint, out numero)) && numero >= 0 && numero <= 10)
+                {
+                    flagnota1 = 1;
+                    nota1 = (byte)numero;
+
+                    do
+                    {
+                        Console.Write("Ingrese un nota2: ");
+                        parseint = Console.ReadLine();
+                        if ((int.TryParse(parseint, out numero)) && numero >= 0 && numero <= 10)
+                        {
+                            flagnota2 = 1;
+                            nota2 = (byte)numero;
+                        }
+                        else
+                        {
+                            Console.WriteLine("ERROR !!");
+                        }
+                    } while (flagnota2 == 0);
+                }
+                else
+                {
+                    Console.WriteLine("ERROR !!");
+                }
+
+            } while (flagnota1 == 0 && flagnota2 == 0);
+
+            student3.Estudiar(nota1, nota2);
+
+            student1.CalcularFinal();
+            student2.CalcularFinal();
+            student3.CalcularFinal();
+            Console.WriteLine(student1.Mostrar());
+            Console.WriteLine(student2.Mostrar());
+            Console.WriteLine(student3.Mostrar());
+            Console.ReadKey();
         }
+            
     }
 }
