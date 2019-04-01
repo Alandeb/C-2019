80afc82a723cf2c743319e8d48df269a49f93d57
Alandeb <40776573+Alandeb@users.noreply.github.com>
2019-04-01 01:47:19 -0300

    Class Boligrafo

---
+++ Ejercicio_17/Ejercicio_17/Boligrafo.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_17
+{
+    public class Boligrafo
+    {
+        const short cantidadTintaMaxima = 100;
+        private ConsoleColor color;
+        private short tinta;
+        public Boligrafo (short tinta , ConsoleColor color)
+        {
+            this.tinta = tinta;
+            this.color = color;
+        }
+        public ConsoleColor GetColor()
+        {
+            return color;
+        }
+        public short GetTinta()
+        {
+            return tinta;
+        }
+
+        private void SetTinta(short tinta)
+        {
+
+            if (tinta <= 100 || tinta >= 0)
+            {
+                this.tinta = tinta;
+            }
+        }
+        public void Recargar()
+        {
+            SetTinta(Boligrafo.cantidadTintaMaxima);
+        }
+
+        public bool Pintar (int gasto,out string dibujo)
+        {
+            do
+            {
+                tinta = GetTinta();
+                if (tinta == 0)
+                {
+                    dibujo = "*"; 
+                    return false;
+                }
+                else
+                {
+                    if (gasto != 0)
+                    {
+                        gasto--;
+                        SetTinta(tinta--);
+                        dibujo = "*";
+                        return true;
+                    }
+                }
+            } while (gasto == 0);
+
+            dibujo = "*";
+            return true;
+        }
+
+    }
+}
---
+++ Ejercicio_17/Ejercicio_17/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boligrafo boligrafo1 = new Boligrafo(100 , ConsoleColor.Blue);
+            Boligrafo boligrafo2 = new Boligrafo(50, ConsoleColor.Red);
+            string dibujo;
+            boligrafo1.Pintar(10, out dibujo);
+
+
+
+            
+        }
+    }
+}
