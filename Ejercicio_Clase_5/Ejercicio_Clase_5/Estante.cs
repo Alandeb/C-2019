a3ffa7f96348ad8c5d0cf0385e0e861e29fd5885
Alan debonis <alan_debonis@hotmail.com>
2019-04-03 21:31:06 -0300

    Ejercicio de clase 5

---
+++ Ejercicio_Clase_5/Ejercicio_Clase_5/Estante.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Clase_5
+{
+    class Estante
+    {
+        private Producto[] productos;
+        private int ubicacionEstante;
+        private Estante(int capacidad)
+        {  // siempre que tenga un array hago esto 
+            this.productos = new Producto[capacidad];
+        }   // siempre que tenga un constructor privado lo llamo de la misma manera
+        public Estante(int capacidad, int ubicacion) : this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+        public Producto[] GetProductos()
+        {
+            return productos;
+        }
+        public string MostrarEstante(Estante e)
+        {
+            string mostrar= "";
+            foreach (Producto p in e.productos)
+            {
+                if (!(object.ReferenceEquals(p, null)))
+                    mostrar += Producto.MostrarProducto(p) + "\n";
+            }
+            return mostrar;
+        }
+        public static bool operator ==(Estante e , Producto p)
+        {
+            
+            bool r = false ;
+            foreach (Producto p2 in e.productos)
+            {
+                if (p2 == p && !(object.ReferenceEquals(p2, null)))
+                {
+                    r = true;
+                    break;
+                }
+            }
+            return r;
+        }
+        public static bool operator !=(Estante e, Producto p)
+        {
+            e.productos.Length//fijarse que mas se puede usar 
+
+            return (!(e == p));
+           
+        }
+       
+        public static bool operator +()
+    }
+}
---
+++ Ejercicio_Clase_5/Ejercicio_Clase_5/Producto.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Clase_5
+{
+    class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+        public Producto (string marca, string codigo , float precio)
+        {
+            codigoDeBarra = codigo;
+            this.marca = marca;
+            this.precio = precio;
+        }
+
+        public string GetMarca()
+        {
+            return marca;
+        }
+        public float GetPrecio()
+        {
+            return precio;
+        }
+        public static explicit operator string (Producto p)
+        {
+            return p.codigoDeBarra;
+        }
+
+        public static string MostrarProducto(Producto p)
+        {
+            return string.Format("El codigo de barra es {0} la marca es {1} " +
+                "y el precio es {2} ", p.codigoDeBarra, p.marca, p.precio);
+        }
+
+        public static bool operator !=(Producto p, string marca)
+        {
+            bool r = false;
+            if (p.marca != marca)
+            {
+                r = true;
+            }
+            return r;
+        }
+        public static bool operator ==(Producto p, string marca)
+        {
+            return (!(p != marca));//uso de operador sobrecargado
+        }
+        public static bool operator !=(Producto p, Producto p2)
+        {
+            bool r = false;
+            if ((p.marca != p2.marca) &&(p.codigoDeBarra != p2.codigoDeBarra))
+            {
+                r = true;
+            }
+            return r;
+        }
+        public static bool operator ==(Producto p, Producto p2)
+        {
+            return (!(p != p2));//uso de operador sobrecargado
+        }
+    }
+}
