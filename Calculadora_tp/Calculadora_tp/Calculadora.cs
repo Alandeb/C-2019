39603bc1bc2f9fc0ec5e0b49c0f36af8b386e5e5
Alandeb <40776573+Alandeb@users.noreply.github.com>
2019-04-07 13:21:02 -0300

    Empieza_Tp

---
+++ Calculadora_tp/Calculadora_tp/Calculadora.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_tp
+{
+    class Calculadora
+    {
+        public double Operar(Numero num1 , Numero num2 , string operador)
+        {
+            return 1;
+        }
+
+        private static string ValidarOperador(string operador)
+        {
+            if (operador != "/" || operador != "+" || operador != "-" || operador != "*") 
+            {
+                operador = "+";
+            }
+
+            return operador;
+        } 
+    }
+}
