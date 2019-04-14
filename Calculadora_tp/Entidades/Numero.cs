62048519ca7a8925f4eee9081afe1c227bd2a33c
Alan_Debonis <alan_debonis@hotmail.com>
2019-04-14 01:23:41 -0300

    tpcalcu

--- Calculadora_tp/Entidades/Numero.cs
+++ Calculadora_tp/Entidades/Numero.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Calculadora_tp
+namespace Entidades
 {
     class Numero
     {
@@ -12,7 +12,7 @@ namespace Calculadora_tp
 
         private string SetNumero
         {
-            
+
             set { numero = ValidarNumero(value); }
         }
         public Numero()
@@ -31,14 +31,14 @@ namespace Calculadora_tp
         private double ValidarNumero(string strNumero)
         {
             double numero;
-            if(double.TryParse(strNumero, out numero))
-			{
-				return numero;
-			}
-			
+            if (double.TryParse(strNumero, out numero))
+            {
+                return numero;
+            }
+            return 0;
         }
 
-        public static double operator +(Numero n1 , Numero n2)
+        public static double operator +(Numero n1, Numero n2)
         {
             return (n1.numero + n2.numero);
         }
@@ -51,15 +51,42 @@ namespace Calculadora_tp
             return (n1.numero * n2.numero);
         }
         public static double operator /(Numero n1, Numero n2)
-        { 
+        {
             return (n1.numero / n2.numero);
         }
 
         public string BinarioDecimal(string binario)
         {
-            return Convert.ToInt64(binario, 2).ToString();
+            Int64 num = Convert.ToInt64(binario, 2);
+            if (num < 0)
+            {
+                return Convert.ToInt64(binario, 2).ToString();
+            }
+            else if(num>0)
+            {
+                return Convert.ToInt64(binario, 2).ToString();
+            }
+            return "Valor invalido";
         }
+        public string DecimalBinario(double numero)
+        {
+            int numInt = (int)numero;
+            if (numInt < 0)
+            {
+                numInt *= -1;
+                return Convert.ToString(numInt, 2);
+            }
+            else if (numInt>0)
+            {
+                return Convert.ToString(numInt, 2);
+            }
 
-
+            return "Valor invalido";
+        }
+        public string DecimalBinario(string numero)
+        {
+            numero = DecimalBinario(Convert.ToDouble(numero));
+            return numero;
+        }
     }
 }
