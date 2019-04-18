be4402898e46c24ec44a668fce80450285112b0c
alan_debonis <alan_debonis@hotmail.com>
2019-04-17 21:42:21 -0300

    Hacer_31

---
+++ Ejercicio_31/Ejercicio_31/Cliente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_31
+{
+    class Cliente
+    {
+        string nombre;
+        int numero;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public int Numero
+        {
+            get { return numero; }
+        }
+        public Cliente (int numero)
+        {
+            this.numero = numero;    
+        }
+
+        public Cliente (int numero , string nombre)
+        {
+            this.numero = numero;
+            Nombre = nombre;
+
+        }
+    }
+}
---
+++ Ejercicio_31/Ejercicio_31/PuestoAtencion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Ejercicio_31
+{
+    public enum Puesto
+    {
+        caja1,
+        caja2
+    }
+
+    class PuestoAtencion
+    {
+        private static int numeroActual;
+        Puesto puesto;
+
+        public int NumeroActual
+        {
+            get { return numeroActual; }
+        }
+        private PuestoAtencion()
+        {
+            numeroActual = 0;
+        }
+        
+        public PuestoAtencion(Puesto puesto)
+        {
+            this.puesto = puesto;
+        }
+        public bool Atencion(Cliente cli)
+        {
+            Thread.Sleep(2000);
+            return true;
+        }
+
+
+    }
+}
