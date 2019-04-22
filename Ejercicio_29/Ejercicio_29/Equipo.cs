667cfce04df260aea30c7da20bfa89414794e444
Alan_Debonis <alan_debonis@hotmail.com>
2019-04-22 16:45:01 -0300

    22/04

---
+++ Ejercicio_29/Ejercicio_29/Equipo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_29
+{
+    class Equipo
+    {
+        private static List<Jugador> jugadores ;
+        private string nombre;
+        private short cantidadDeJugadores;
+
+        private Equipo()
+        {
+            jugadores = new List<Jugador>();
+        }
+        public Equipo (short cantidad ,string nombre)
+        {
+            this.cantidadDeJugadores = cantidad;
+            this.nombre = nombre;
+        }
+        public static bool operator +(Equipo e,Jugador j)
+        {
+
+        }
+    }
+}
---
+++ Ejercicio_29/Ejercicio_29/Jugador.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_29
+{
+    class Jugador
+    {
+        private  int dni;
+        private  int partidosJugados;
+        private  int totalGoles;
+        private  string nombre;
+        private  static float promedioGoles;
+
+        private Jugador()
+        {
+            partidosJugados = 0;
+            totalGoles = 0;
+            promedioGoles = 0;
+        }
+
+        public Jugador(int dni,string nombre)
+        {
+            this.dni = dni;
+            this.nombre = nombre;
+
+        }
+        public  Jugador(int dni, string nombre,int totalGoles,int totalPartidos)
+        {
+            this.dni = dni;
+            this.nombre = nombre;
+            this.totalGoles = totalGoles;
+            partidosJugados = totalPartidos;
+        }
+        public float GetPromedioGoles()
+        {
+            if (partidosJugados>0 && totalGoles>0)
+                promedioGoles = totalGoles / partidosJugados;
+            return promedioGoles;
+        }
+        public string MostrarDatos()
+        {
+            return string.Format("Jugador: {0} , DNI: {1} ,Partidos Jugados: {2},Goles Totales: {3} , Promedio de Goles {4}", nombre, dni, partidosJugados, totalGoles, promedioGoles);
+        }
+        public static bool operator ==(Jugador j1, Jugador j2)
+        {
+            bool retorno = false;
+            if(j1.dni == j2.dni)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+        public static bool operator !=(Jugador j1, Jugador j2)
+        {
+            return !(j1 == j2);
+        }
+
+    }
+}
