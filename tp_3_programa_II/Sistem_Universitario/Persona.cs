ae65452ed401adffa65ff495b45447c377fc3d42
Alan debonis <alan_debonis@hotmail.com>
2019-06-05 22:10:23 -0300

    Tp 3 inicio

---
+++ tp_3_programa_II/Sistem_Universitario/Persona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistem_Universitario
+{
+    public abstract class Persona
+    {
+        public enum Enacionalidad
+        {
+            Argentino,
+            Extranjero
+        }
+        private string apellido;
+        private int dni;
+        private Enacionalidad nacionalidad;
+        private string nombre;
+
+        abstract public string Apellido { get; set; }
+        
+    }
+}
